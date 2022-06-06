795b62a06897d072ba58cd08482d17713789b7db
phil <p@pkp.dev>
2022-06-06 07:34:22 +0200

    sortable table

--- ipn/ipnlocal/assets/src/index.js
+++ ipn/ipnlocal/assets/src/index.js
@@ -3,15 +3,7 @@ let data
 fetch('/json')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        // d = data.Peers.map(x=>{
-        //     console.log([x.HostName, x.OS])
-        //     return [
-        //         x.HostName, 
-        //         x.IPv4,
-        //         x.OS
-        //     ]
-        // })
+        // console.log(data)
         newGrid(data.Peers)
     });
 
@@ -48,14 +40,8 @@ newGrid = d => {
             },
         ],
         sort: true,
+        search: true,
         data: d
-        // data: [
-        //   ["John", "john@example.com", "(353) 01 222 3333"],
-        //   ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-        //   ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-        //   ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-        //   ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
-        // ]
     }).render(document.getElementById("wrapper"));
 }
 
