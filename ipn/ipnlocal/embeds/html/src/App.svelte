759a64cad80db6230705b02dfcc68b13a0dc3fe8
phil <p@pkp.dev>
2022-06-16 17:33:31 +0200

    updates

--- ipn/ipnlocal/embeds/html/src/App.svelte
+++ ipn/ipnlocal/embeds/html/src/App.svelte
@@ -1,23 +1,13 @@
 <script lang="ts">
-    // import { onMount } from "svelte"
-
     import NodeInfo from "./lib/Nodeinfo.svelte"
     import Peers from './lib/Peers.svelte'
 </script>
 
-<!-- <header class="flex bg-gray-100 items-center py-2 mb-8"> -->
+<header class=" bg-gray-100 items-center py-2 mb-8">
     <NodeInfo />
     <Peers />
-<!-- </header> -->
+</header>
 
 <style>
   @import "../../local.css";
-
-  /* :root {
-    font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
-    font-size: 100%;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  } */
-
 </style>
