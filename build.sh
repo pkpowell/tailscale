f33f14367a985225a8e426d703efeafa9ac675fb
phil <phil@ph.lan>
2022-06-22 17:59:12 +0200

    updates

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
+npm --prefix ipn/ipnlocal/embeds/html i > /dev/null && {
+    echo "npm install ok"
+} || {
+    echo "npm install failed"
+    exit $?
+}
+
 npm --prefix ipn/ipnlocal/embeds/html run build > /dev/null && {
-    echo "npm build succeeded"
+    echo "npm build ok"
 } || {
     echo "npm build failed"
     exit $?
 }
 
 sh ./build_dist.sh tailscale.com/cmd/tailscaled && {
-    echo "tailscale.com/cmd/tailscaled built"
+    echo "tailscale.com/cmd/tailscaled ok"
 
 } || {
     echo "build tailscale.com/cmd/tailscaled failed"
@@ -18,7 +25,7 @@ sh ./build_dist.sh tailscale.com/cmd/tailscaled && {
 
 
 sh ./build_dist.sh tailscale.com/cmd/tailscale && {
-    echo "tailscale.com/cmd/tailscale built"
+    echo "tailscale.com/cmd/tailscale ok"
 
 } || {
     echo "build tailscale.com/cmd/tailscale failed"
